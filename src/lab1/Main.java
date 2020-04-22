65ee62b571ea8235df77e8a97692d316a80502a9
unknown <dns.lavinia@gmail.com>
2020-04-22 16:56:55 +0300

    mend

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -1,4 +1,8 @@
 package lab1;
+import lab1.home.Challenge1;
+import lab1.home.Challenge2;
+import lab1.home.Challenge3;
+import lab1.home.Challenge4;
 
 public class Main {
     private static int semester = 2;
@@ -14,6 +18,23 @@ public class Main {
         System.out.println("result= " + cf.addManyTimes());
 
         int nr=6;
-        cf.Autobox(nr);
+        //cf.Autobox(nr);
+
+        Challenge1 c1 = new Challenge1() ;
+        c1.challenge1();
+
+        Challenge2 c2 = new Challenge2();
+        for(int i=1; i<=53; i++)
+        {
+            System.out.println(c2.compute(i));
+        }
+
+        Challenge3 c3 = new Challenge3();
+        int arr[] = {3,2,-3,-2,3,0};
+        System.out.println(c3.pairOfTwo(arr));
+
+        Challenge4 c4 = new Challenge4();
+        int arr2[] = {3,2,-3,-5,3};
+        System.out.println(c4.pairOfThree(arr2));
     }
 }
